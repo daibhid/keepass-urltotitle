2a3fdfad943c6c9286404ee4bfebf3ee9ba3d8d0
David Dunsmore <dynamite.a.d@gmail.com>
2020-04-04 22:21:19 +0100

    Add a testing project to check the suggestions

---
+++ URLInNameTests/URLInNameExtTests.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="URLInNameExtTests.cs" company="daibhid">
+// Copyright (c) daibhid. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace URLInName.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class URLInNameExtTests
+    {
+        [Test]
+        public void SuggestModificationTest()
+        {
+            this.TestSuggestion("https://www.somesite.com/about/index.html", "somesite.com", "https://www.somesite.com");
+            this.TestSuggestion("twitch.tv", "twitch.tv", "https://twitch.tv");
+        }
+
+        private void TestSuggestion(string url, string newName, string newURL)
+        {
+            SuggestedModification suggestedModification = URLInNameExt.SuggestModification(url, "oldName", null);
+            Assert.AreEqual(newName, suggestedModification.NewTitle);
+            Assert.AreEqual(newURL, suggestedModification.SuggestedUrl);
+        }
+    }
+}
